787d1a891768c058e4e916d6cc3cae66187e7e9c
ckzttm2j7d-design <ckzttm2j7d@privaterelay.appleid.com>
2026-05-29 10:29:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 
-function buyAccount(username,password){
-  alert(
-    "Thanh toán thành công!\n\n" +
-    "Tài khoản: " + username + "\n" +
-    "Mật khẩu: " + password
-  );
-}
-
 function openLogin(){
-  document.getElementById("loginModal").style.display = "flex";
+document.getElementById('loginModal').style.display='flex';
 }
 
 function closeLogin(){
-  document.getElementById("loginModal").style.display = "none";
-  alert("Đăng nhập thành công!");
+document.getElementById('loginModal').style.display='none';
+alert('Đăng nhập thành công');
+}
+
+function openAdmin(){
+document.getElementById('adminModal').style.display='flex';
+}
+
+function closeAdmin(){
+document.getElementById('adminModal').style.display='none';
+}
+
+function fakePay(user,pass){
+alert(
+'Thanh toán MoMo thành công!\n\n' +
+'Tài khoản: ' + user + '\n' +
+'Mật khẩu: ' + pass
+);
+}
+
+function fakeNap(){
+alert('Đã mở thanh toán MoMo giả lập');
+}
+
+function randomAcc(){
+const list=[
+'Random acc huyền thoại',
+'Random acc VIP',
+'Random acc cực hiếm'
+];
+
+const pick=list[Math.floor(Math.random()*list.length)];
+
+alert('Anh nhận được: ' + pick);
 }
